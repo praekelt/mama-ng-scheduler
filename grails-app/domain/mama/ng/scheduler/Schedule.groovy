4331303249f42b3e999133b653b9cb4011f47ff4
Lyndsay <lyndsaygaynor@gmail.com>
2015-03-09 13:09:02 +0200

    Created Schedule domain object. Added validation for cron with regex.

---
+++ grails-app/domain/mama/ng/scheduler/Schedule.groovy
@@ -0,0 +1,27 @@
+package mama.ng.scheduler
+
+class Schedule {
+
+    String id = UUID.randomUUID().toString()
+    String userId
+    Integer frequency
+    Integer sendCounter = 0
+    String cronDefinition
+    Date nextSend
+    String endpoint
+    Date created
+
+    static constraints = {
+        id generator:'assigned'
+        userId(blank: false)
+        frequency(min: 1)
+        sendCounter()
+        cronDefinition(matches: '/^(((([1-5]?[0-9])(\\,|\\-))?([1-5]?[0-9]))|\\*)\\ ' + //min (0 - 59)
+            '((((([1]?[0-9])|([2][0-3]))(\\,|\\-))?(([1]?[0-9])|([2][0-3])))|\\*)\\ ' + //hour (0 - 23)
+            '((((([1-9])|([1-2][0-9])|([3][0-1]))(\\,|\\-))?(([1-9])|([1-2][0-9])|([3][0-1])))|\\*)\\ ' + //day of month (1 - 31)
+            '((((([0-9])|([1][0-2]))(\\,|\\-))?(([0-9])|([1][2])))|\\*)\\ ' + //month (1 - 12)
+            '(((([0-6])(\\,|\\-))?([0-6]))|\\*)$/') //day of week (0 - 6) (0 to 6 are Sunday to Saturday, or use names; 7 is Sunday, the same as 0)
+        nextSend()
+        endpoint()
+    }
+}
