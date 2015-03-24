a0ab2bd8ca4d0f90467b3f721ecc0ebfdbd5310a
Lyndsay <lyndsaygaynor@gmail.com>
2015-03-24 10:15:24 +0200

    indentation and comments

--- test/unit/mama/ng/scheduler/CronParserServiceSpec.groovy
+++ test/unit/mama/ng/scheduler/CronParserServiceSpec.groovy
@@ -18,109 +18,112 @@ class CronParserServiceSpec extends Specification {
 
     void "test next date 0 9 * * 1 basic"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
-        Date nextDate = service.determineNextDate("0 9 * * 1", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
+            Date nextDate = service.determineNextDate("0 9 * * 1", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,4,9,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,4,9,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 9 * * 1 complex"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,30,0,0,0);
-        Date nextDate = service.determineNextDate("0 9 * * 1", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,30,0,0,0);
+            Date nextDate = service.determineNextDate("0 9 * * 1", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,1,1,9,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,1,1,9,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 9 2 * * basic"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
-        Date nextDate = service.determineNextDate("0 9 2 * *", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
+            Date nextDate = service.determineNextDate("0 9 2 * *", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,2,9,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,2,9,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 9 2 * * complex"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,30,0,0,0);
-        Date nextDate = service.determineNextDate("0 9 2 * *", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,30,0,0,0);
+            Date nextDate = service.determineNextDate("0 9 2 * *", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,1,2,9,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,1,2,9,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 11 * * 2,4 basic"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
-        Date nextDate = service.determineNextDate("0 11 * * 2,4", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
+            Date nextDate = service.determineNextDate("0 11 * * 2,4", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,5,11,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,5,11,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 11 * * 2,4 complex"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,27,0,0,0);
-        Date nextDate = service.determineNextDate("0 11 * * 2,4", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,27,0,0,0);
+            Date nextDate = service.determineNextDate("0 11 * * 2,4", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,28,11,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,28,11,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 10 1-7 * 2 basic"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
-        Date nextDate = service.determineNextDate("0 10 1-7 * 2", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,1,0,0,0);
+            Date nextDate = service.determineNextDate("0 10 1-7 * 2", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,5,10,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,5,10,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 10 1-7 * 2 complex"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,27,0,0,0);
-        Date nextDate = service.determineNextDate("0 10 1-7 * 2", new Date(calendar.getTimeInMillis()))
+            Calendar calendar = new GregorianCalendar(2015,0,27,0,0,0);
+            Date nextDate = service.determineNextDate("0 10 1-7 * 2", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,1,2,10,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            Calendar expectedCalendar = new GregorianCalendar(2015,1,2,10,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 
     void "test next date 0 10 1-30 * 2,4 complex"() {
         given:
-        Calendar calendar = new GregorianCalendar(2015,0,6,0,0,0);
-        Date nextDate = service.determineNextDate("0 10 1-30 * 2,4", new Date(calendar.getTimeInMillis()))
+            // January 6 2015, Tuesday
+            Calendar calendar = new GregorianCalendar(2015,0,6,0,0,0);
+            // 1-30 of the month on Monday or Wednesday
+            Date nextDate = service.determineNextDate("0 10 1-30 * 2,4", new Date(calendar.getTimeInMillis()))
 
-        Calendar expectedCalendar = new GregorianCalendar(2015,0,7,10,0,0);
-        Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
+            // January 7 2015, Wednesday
+            Calendar expectedCalendar = new GregorianCalendar(2015,0,7,10,0,0);
+            Date expectedDate = new Date(expectedCalendar.getTimeInMillis())
 
         expect:
-        nextDate == expectedDate
+            nextDate == expectedDate
     }
 }
