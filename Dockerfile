ec5900f5b480b7ff076f145601937e4250fe63c3
Simon de Haan <simon@praekeltfoundation.org>
2015-03-30 22:34:03 +0200

    this seems to work but we will find out on the dev-lyndsay machine

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,5 @@ MAINTAINER Praekelt Foundation <dev@praekeltfoundation.org>
 COPY . /app
 WORKDIR /app
 
-RUN grails refresh-dependencies
 RUN grails prod war
 CMD ["prod", "run-war"]
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,8 +3,21 @@
 docker stop mama-ng-scheduler
 docker rm mama-ng-scheduler
 
+docker stop postgresql
+docker rm postgresql
+
 # Exit on errors from here.
 set -e
 
+docker run -d --name="postgresql" \
+    -v `pwd`/data:/data:rw \
+    -e USER="postgres" \
+    -e DB="scheduler" \
+    -e PASS="postgres" \
+    paintedfox/postgresql
+
 docker build -t praekelt/mama-ng-scheduler $REPO
-docker run -d --name=mama-ng-scheduler praekelt/mama-ng-scheduler
+docker run -d --name=mama-ng-scheduler \
+    --link=postgresql:postgresql \
+    -e DATABASE_URL="postgresql://postgresql:5432/scheduler" \
+    praekelt/mama-ng-scheduler
