9e43e56966197903c3c6ff415d76162f77fe379f
Simon de Haan <simon@praekeltfoundation.org>
2015-04-01 17:41:57 +0200

    docker processes now run by supervisord

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-docker stop mama-ng-scheduler
-docker rm mama-ng-scheduler
-
-docker stop postgresql
-docker rm postgresql
-
-# Exit on errors from here.
+# # Exit on errors from here.
 set -e
 
-docker run -d --name="postgresql" \
-    -v $INSTALLDIR/$REPO/postgres-data:/var/lib/postgresql/data:rw \
-    -e POSTGRES_USER="scheduler" \
-    -e POSTGRES_PASSWORD="scheduler" \
-    postgres
-
 docker build -t praekelt/mama-ng-scheduler $INSTALLDIR/$REPO
-docker run -d --name=mama-ng-scheduler \
-    --link=postgresql:postgresql \
-    -p 8090:8090 \
-    -e DATABASE_URL="postgresql://scheduler:scheduler@postgresql:5432/scheduler" \
-    praekelt/mama-ng-scheduler
